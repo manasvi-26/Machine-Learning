84f114e52c3cecd4dce7125ff13baf611e53259d
Lenovo <poojadesur@gmail.com>
2021-02-25 11:02:22 +0530

    feb 24 - min error seems reasonable

--- A2/codes/24-2.py
+++ A2/codes/24-2.py
@@ -2,12 +2,15 @@ import numpy as np
 import random
 from client import *
 import json
+import matplotlib.pyplot as plt
+
 
 overfit = np.array([0.0, -1.45799022e-12, -2.28980078e-13,  4.62010753e-11, -1.75214813e-10, -1.83669770e-15,
            8.52944060e-16,  2.29423303e-05, -2.04721003e-06, -1.59792834e-08,  9.98214034e-10])
 
 POP = 10
 FEATURE = 11
+ITERATIONS = 40
 
 def generate_initial(overfit, prob):
 
@@ -35,7 +38,6 @@ def generate_initial(overfit, prob):
     return generation
 
 
-
 # CALL SERVER:
 
 def call_server(generation):
@@ -43,18 +45,18 @@ def call_server(generation):
     error = np.zeros(shape = (POP, 2))
     
     
-    # for i in range(len(generation)):
+    for i in range(len(generation)):
         
 
-    #    curr_error = get_errors(SECRET_KEY,generation[i].tolist())
-    #    error[i] = np.array(curr_error)
+       curr_error = get_errors(SECRET_KEY,generation[i].tolist())
+       error[i] = np.array(curr_error)
 
 
 
-    for i in range(len(generation)):
+    # for i in range(len(generation)):
 
-        curr_error = [random.uniform(1, 10), random.uniform(1, 10)]
-        error[i] = curr_error
+    #     curr_error = [random.uniform(1, 10), random.uniform(1, 10)]
+    #     error[i] = curr_error
 
     return error
     
@@ -79,8 +81,6 @@ def sort_generation(generation,fitness):
     generation = generation[sorted_idx]
 
     fitness = np.sort(fitness)
-
-
 
     return generation,fitness
 
@@ -166,54 +166,31 @@ def mutation(crossOver_generation, mutation_prob):
 
 def create_new_gen(generation,mutated_generation,fitness,child_fitness):
 
-    new_generation = np.zeros(shape = POP + int(POP/2) )
+    new_generation = np.zeros(shape = (POP,FEATURE ))
+    new_fitness = np.zeros(shape = POP )
+    # new_errors = np.zeros(shape = (POP,2) )
+
 
     # parent top5 pool and mutated generations fitness
-    both_fitness = np.concatenate( (generation[:5],mutated_generation), axis=0)
+    pot_new_generation  = np.concatenate( (generation[:5],mutated_generation), axis=0)
 
     # parent top5 vectors and mutated generation vectors
-    pot_new_generation = np.concatenate( (fitness[:5],child_fitness), axis = 0)
+    pot_new_generation_fitness = np.concatenate( (fitness[:5],child_fitness), axis = 0)
 
-    top10 = sort_generation(pot_new_generation, both_fitness)
+    top10,top10fitness = sort_generation(pot_new_generation, pot_new_generation_fitness)
+
 
     new_generation = top10[:10]
+    new_fitness = top10fitness[:10]
 
-    return new_generation
+    return new_generation,new_fitness
 
-# INITIAL POPULATION
-
-
-# def Convert(v):
-#     print(type(v))
-
-#     # for i in len(range(v)):
-#     #     print(v[i])
-#     #     #v[i] = v[i].tolist()
-    
-#     new_v = v.tolist()
-
-#     return new_v
-
-
-
-# for i in range(3):
-
-#     generation = generate_initial(overfit, 0.9, 0.1)
-
-#     generation = Convert(generation)
-
-#     print(type(generation))
-    
-#     with open('../output_files/temp.txt', 'a+') as write_file:
-#         write_file.write("\n")
-#         write
-#         json.dump(generation,write_file)
 
 
 def write_to_file(vector, text, iter):
 
 
-    with open('../output_files/24-3.txt', 'a+') as write_file:
+    with open('../output_files/24-2.txt', 'a+') as write_file:
 
         if(text == 'generation'):
             write_file.write("\n\n")
@@ -228,38 +205,38 @@ def write_to_file(vector, text, iter):
         json.dump(vector.tolist(),write_file)
             
 
+# INITIAL POPULATION
 generation = generate_initial(overfit, 0.9)
+error = call_server(generation)
+fitness = fitness_function(error)
 
+min_error = np.zeros(shape =ITERATIONS)
+best_vector = np.zeros(shape = (ITERATIONS,11))
 
-for iter in range(24):
+for iter in range(ITERATIONS):
 
     write_to_file(generation,'generation',iter)
 
-    # call server
-    error = call_server(generation)
-    write_to_file(error,'error',iter)
-
-    # call fitness - sum of errors 
-    fitness = fitness_function(error)
+    # write_to_file(error,'error',iter)
 
     #sort generation in order of fitness and sort fitness
     generation,fitness = sort_generation(generation,fitness)
- 
+
+    min_error[iter] = fitness[0]
+    best_vector[iter] = generation[0]
+
     # call selection
     pool = selection(generation)
     write_to_file(pool,'pool',iter)
-
 
     # call crossover
     crossOver_generation = crossover(pool)
     write_to_file(crossOver_generation,'crossOver_generation',iter)
 
-
     # call mutation
     mutated_generation = mutation(crossOver_generation,0.7)
     write_to_file(mutated_generation,'mutated_generation',iter)
 
-   
     # call get_errors for mutated generation
     child_errors = call_server(mutated_generation)
 
@@ -268,34 +245,36 @@ for iter in range(24):
     child_fitness = fitness_function(child_errors)
 
     # take top 10 from ( mutated generation + top 5 of parents )
-    new_generation = create_new_gen(generation,mutated_generation,fitness,child_fitness)
-
+    new_generation,new_fitness= create_new_gen(generation,mutated_generation,fitness,child_fitness)
 
 
     generation = new_generation
+    fitness = new_fitness
     
+min_error[ITERATIONS - 1] = fitness[0]
+best_vector[ITERATIONS - 1] = generation[0]
+iters = range(ITERATIONS)
 
-    
-# #server call for final generation
-with open('../output_files/23-final.txt', 'a+') as write_file:
+plt.plot(iters,min_error)
+plt.xlabel("iterations")
+plt.ylabel("inimum fitness (sum of errors)")
+plt.show()
+
+print(min_error[ITERATIONS -1])
+print(best_vector[ITERATIONS -1])
+
+with open('../output_files/24-final.txt', 'a+') as write_file:
+    write_file.write("\nFinal Generation\n")
     json.dump(generation.tolist(),write_file)
-    write_file.write("\nERRRORSS\n")
+    write_file.write("\n\n")
+    write_file.write("\nAll min errors\n")
+    json.dump(min_error.tolist(),write_file)
+    write_file.write("\n\n")
+    write_file.write("\nAll Best Vectors\n")
+    json.dump(best_vector.tolist(),write_file)
+    write_file.write("\n\n")
 
-error = call_server(generation)
-
-
-
-with open('../output_files/23-final.txt', 'a+') as write_file:
-    json.dump(error.tolist(),write_file)
-
-min_error =1e15
-min_index = 0
-for i in range(len(error)):
-    if(min_error  > (error[i][0] +error[i][1])):
-        min_error=error[i][0] +error[i][1]
-        min_index = i
-
-print(min_error)
-print(generation[min_index])
+    
 
 
+    
--- A2/codes/test.py
+++ A2/codes/test.py
@@ -1,20 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 arr1 = np.array([3, 5, 1, 2, 7, 9])
 arr2 = np.array([2, 4, 7, 6, 8, 1])
 # arr2 = arr1 * 10
-sorted_idxs = np.argsort(arr1)
-print(sorted_idxs)
+# sorted_idxs = np.argsort(arr1)
+# print(sorted_idxs)
 
-print(arr2[sorted_idx])
-# >>> array([2, 1, 0, 4, 3])
+# print(arr2[sorted_idx])
+# # >>> array([2, 1, 0, 4, 3])
 
-print(arr1[sorted_idxs])
-# >>> array([ 1,  3,  4, 21, 32])
+# print(arr1[sorted_idxs])
+# # >>> array([ 1,  3,  4, 21, 32])
 
-print(arr2[sorted_idxs])
-# >>> array([ 10,  30,  40, 210, 320])
+# print(arr2[sorted_idxs])
+# # >>> array([ 10,  30,  40, 210, 320])
 
 
+iters = range(6)
 
-2 4 7 6 8 2
+plt.plot(arr1,iters)
+plt.show()
+
